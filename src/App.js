b6757977a8459092e319d251d8c9f83708a3108c
sonu <sonu444.karmacharya@gmail.com>
2020-02-08 15:35:13 +0545

    modified

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
 import React, { Component } from 'react'
 import Login from './Login';
-import Home from './Home';
+import Register from './Register';
+import {BrowserRouter, Link,Route} from 'react-router-dom'
 import fire from './config/fire';
 
 class App extends Component{
-
-	constructor(props){
-		super(props);
-		this.state = {
-			user :null,
-		}
-		this.authListener = this.authListener.bind(this);
-	}
-
-	componentDidMount(){
-		this.authListener();
-	}
-
-	authListener(){
-		fire.auth().onAuthStateChanged((user) =>{
-			if(user){
-				this.setState({user});
-			}
-			else{
-				this.setState({user:null});
-			}
-		})
-
-	}
-	render(){
-		return(
-				<div>
-					{this.state.user ?  (<Home/>) : (<Login/>)}
+		render(){
+			return(
+				<div>				
+				<BrowserRouter>
+                <h2>
+                  <Link to={"/login"}> Login</Link>
+                </h2>
+                  <Route exact path="/login" component={Login}/>
+                </BrowserRouter>
 				</div>
 
 			
-		)
+			);
+		}
 	}
-}
 
 export default App;
--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import firebase from 'firebase'
-import { Link, BrowserRouter,Route } from 'react-router-dom';
-import Register from './Register'
-class Login extends React.Component{
+import { BrowserRouter, Route ,Link } from 'react-router-dom';
+import Login from './Login';
+class Register extends React.Component{
 	constructor(props){
 		super(props);
 		this.state = {
@@ -17,28 +17,37 @@ class Login extends React.Component{
 	}
 	handleSubmit = e => {
 		e.preventDefault();
-    const {email, password} = this.state;
+    const {email, username, password} = this.state;
     firebase
         .auth()
-        .signInWithEmailAndPassword(email, password)
-        .then(user => {
-            this.props.history.push('/');
+	.createUserWithEmailAndPassword(email, password)
+	.then(() => {
+	    const user = firebase.auth().currentUser;
+	    user
+	        .updateProfile({displayName: username})
+		.then(() => {
+		    this.props.history.push('/');
+		})
+		.catch(error => {
+		    this.setState({error});
+		});
         })
-        .catch(error => {
-            this.setState({error});
-        });
+    .catch(error => {
+        this.setState({error});
+    });
 	}
 	render(){
 		const {email, username, password, error} = this.state;
 		return(
 			<div className="auth--container">
-				
-				<h1>Login your account</h1>
+				<h1>Register your account</h1>
 				{error && <p className="error-message">{error.message}</p>}
 				<form onSubmit={this.handleSubmit}>
+					<label htmlFor="username">Username</label>
+					<input type="text" name="username" id="username" value={username} onChange={this.handleChange} />
 					<label htmlFor="email">Email address</label>
 					<input type="text" name="email" id="email" value={email} onChange={this.handleChange} />
-					<label htmlFor="password"> password</label>
+					<label htmlFor="password">Choose a password</label>
 					<input
 						type="password"
 						name="password"
@@ -47,15 +56,9 @@ class Login extends React.Component{
 						onChange={this.handleChange}
 					/>
 					<button className="general-submit" children="Get Started" />
-					<BrowserRouter>	
-				<h2>
-					<Link to={"/register"}> Register</Link>
-                </h2>
-                  <Route exact path="/register" component={Register}/>
-                </BrowserRouter>
 				</form>
 			</div>
 		);
 	}
 }
-export default Login;
+export default Register;
